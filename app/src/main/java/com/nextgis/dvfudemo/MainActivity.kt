59a7c1a079c4102cbdb7e2e66ac3ccf873bfa690
Stanislav Petryakov <become.glory@yandex.ru>
2019-03-20 19:10:48 +0300

    Add map to activity

--- app/src/main/java/com/nextgis/dvfudemo/MainActivity.kt
+++ app/src/main/java/com/nextgis/dvfudemo/MainActivity.kt
@@ -1,12 +1,53 @@
+/*
+ * Project:  Demo DVFU
+ * Purpose:  NextGIS demo project for DVFU
+ * Author:   Stanislav Petriakov, becomeglory@gmail.com
+ * ****************************************************************************
+ * Copyright (c) 2019 NextGIS, info@nextgis.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.nextgis.dvfudemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import com.nextgis.maplib.api.IGISApplication
+import com.nextgis.maplib.map.MapDrawable
+import com.nextgis.maplibui.mapui.MapViewOverlays
+
+
 
 class MainActivity : AppCompatActivity() {
+    private var map: MapViewOverlays? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val app = application as? IGISApplication
+        map = MapViewOverlays(this, app?.map as MapDrawable?)
+        map?.id = R.id.map
+
+        val container = findViewById<FrameLayout>(R.id.map)
+        container.addView(map)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val container = findViewById<FrameLayout>(R.id.map)
+        container.removeView(map)
     }
 }
