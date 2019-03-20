66287a7e5734dfc7da2d480adff4a4f070b9aa4d
Stanislav Petryakov <become.glory@yandex.ru>
2019-03-20 19:09:24 +0300

    Add application class

---
+++ app/src/main/java/com/nextgis/dvfudemo/DVFUApplication.kt
@@ -0,0 +1,51 @@
+/*
+ * Project:  Demo DVFU
+ * Purpose:  NextGIS demo project for DVFU
+ * Author:   Stanislav Petriakov, becomeglory@gmail.com
+ * ****************************************************************************
+ * Copyright (c) 2019 NextGIS, info@nextgis.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.nextgis.dvfudemo
+
+import com.nextgis.maplibui.GISApplication
+
+class DVFUApplication : GISApplication() {
+    override fun getAuthority(): String {
+        return AUTHORITY
+    }
+
+    override fun showSettings(setting: String?) {
+
+    }
+
+    override fun sendEvent(category: String?, action: String?, label: String?) {
+
+    }
+
+    override fun sendScreen(name: String?) {
+
+    }
+
+    override fun getAccountsType(): String {
+        return ACCOUNT_TYPE
+    }
+
+    companion object {
+        const val ACCOUNT_TYPE = "dvfu_account"
+        const val AUTHORITY = "com.nextgis.dvfudemo.provider"
+    }
+}
